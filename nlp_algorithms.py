867721eb333f1b5c85d962485873e47517655962
lucascouri2 <lncc2@cin.ufpe.br>
2022-10-27 16:01:20 -0300

    Pronto pra rodar

--- nlp_algorithms.py
+++ nlp_algorithms.py
@@ -5,6 +5,7 @@ import time
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity
 
 from scipy import spatial
 
@@ -15,6 +16,29 @@ from nltk.tokenize import word_tokenize
 
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
+from sklearn.model_selection import train_test_split
+from sentence_transformers import evaluation
+from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample, losses
+
+from torch.utils.data import DataLoader
+
+SEED = 42
+
+'''import pandas as pd
+import numpy as np
+import time
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+from scipy import spatial
+
+from sentence_transformers import evaluation
+from sentence_transformers import SentenceTransformer, SentencesDataset, InputExample, losses
+from torch.utils.data import DataLoader
+
+from sklearn.model_selection import train_test_split
+
+SEED = 42'''
 
 
 def join_docs(df_text, column1, column2, unique = False):
@@ -104,7 +128,7 @@ def apply_tf_idf(df, len_pipeline):
 
 
 ###Bert###
-def get_bert(model, doc1, doc2):
+'''def get_bert(model, doc1, doc2):
     
     data = [doc1, doc2]
     sentence_embeddings = model.encode(data)
@@ -127,8 +151,74 @@ def apply_bert(df, len_pipeline, model, model_name):
         df_bert[f'bert_{model_name}{index}'] = df.apply(lambda row: get_bert(model, " ".join(row[f'doc1_pipeline{index}']), " ".join(row[f'doc2_pipeline{index}'])), axis=1)
         time_list.append((f'bert_{model_name}{index}', time.time()-start_time))
 
-    return (df_bert, pd.DataFrame(time_list))
+    return (df_bert, pd.DataFrame(time_list))'''
 
+###Bert###
+def get_bert(model, doc1, doc2):
+    
+    data = [doc1, doc2]
+    sentence_embeddings = model.encode(data)
+
+    infer1 = sentence_embeddings[0]
+    infer2 = sentence_embeddings[1]
+    
+    cos_similarity = 1 - spatial.distance.cosine(infer1, infer2) #de 0 a 1
+    
+    return cos_similarity
+
+def apply_bert(df, df_test, len_pipeline, model, model_name, fine_tune = False):
+
+    train_batch_size = 1
+    num_epochs = 2
+    model_save_path = 'output/fine_tune_bert-'+model_name
+
+    time_list = []
+
+    df_bert = pd.DataFrame()
+    for index in range(len_pipeline):
+        start_time = time.time()
+        if fine_tune==True:
+
+            train_samples = []
+            valid_samples = []
+
+            X = df[[f'doc1_pipeline{index}', f'doc2_pipeline{index}']]
+            y = df['Overall']
+
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size = 0.2, random_state = SEED)
+
+            train_df = pd.concat([X_train, y_train], axis=1)
+            train_df = train_df.reset_index()
+            valid_df = pd.concat([X_valid, y_valid], axis=1)
+            valid_df = valid_df.reset_index()
+
+
+            ### testar sem tokenizar
+            for i in range(1, len(train_df)):
+                inp_example = InputExample(texts=[train_df.iloc[i][f'doc1_pipeline{index}'], train_df.iloc[i][f'doc2_pipeline{index}']], label=float(train_df.iloc[i]['Overall']))
+                train_samples.append(inp_example)
+
+            for j in range(1, len(valid_df)):
+                inp_example = InputExample(texts=[valid_df.iloc[j][f'doc1_pipeline{index}'], valid_df.iloc[j][f'doc2_pipeline{index}']], label=float(valid_df.iloc[j]['Overall']))
+                valid_samples.append(inp_example)
+
+            train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size)
+            train_loss = losses.CosineSimilarityLoss(model=model)
+            evaluator = evaluation.EmbeddingSimilarityEvaluator.from_input_examples(valid_samples)#(sts_reader.get_examples('sts-dev.csv'))
+
+            model.fit(train_objectives=[(train_dataloader, train_loss)],
+                    evaluator=evaluator,
+                    epochs=num_epochs,
+                    evaluation_steps=1000,
+                    #warmup_steps=warmup_steps, 100?
+                    output_path=model_save_path
+                    )
+
+        ## ALTERAR: Avaliar no dataset de test
+        df_bert[f'bert_{model_name}{index}'] = df_test.apply(lambda row: get_bert(model, " ".join(row[f'doc1_pipeline{index}']), " ".join(row[f'doc2_pipeline{index}'])), axis=1)
+        time_list.append((f'bert_{model_name}{index}', time.time()-start_time))
+
+    return (df_bert, pd.DataFrame(time_list))
 
 
 ###Doc2Vec###
